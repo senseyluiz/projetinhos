6d8e091b069441f4568655f0f9de392600ad05c6
Luis Henrique <senseyluiz@gmail.com>
2021-11-22 10:48:01 -0300

    Corrigindo valor de input

--- Mini-Caixa/login/scriptlogin.js
+++ Mini-Caixa/login/scriptlogin.js
@@ -1,18 +1,24 @@
 let logar = document.querySelector("#logar");
-let cadastrar = document.querySelector("cadastrar");
-let usuario = document.querySelector("#usuario").value;
-let senhaUsuario = document.querySelector("#senhaUsuario").value;
+let cadastrar = document.querySelector("#cadastrar");
+let usuario = document.querySelector("#usuario");
+let senhaUsuario = document.querySelector("#senhaUsuario");
+
 
 logar.addEventListener("click", function(e) {
     e.preventDefault();
     let nickName = localStorage.getItem("Nickname")
     let senha = localStorage.getItem("Senha");
+    console.log("Local Storage")
     console.log("Usuario:" + nickName)
     console.log("Senha Usuario:" + senha)
 
-    // if (nickName == usuario && senha == senhaUsuario) {
-    //     alert("Senha Correta");
-    // } else {
-    //     alert("Senha Incorreta");
-    // }
+    console.log("Digitado")
+    console.log("Usuario:" + usuario.value)
+    console.log("Senha Usuario:" + senhaUsuario.value)
+
+    if (nickName == usuario.value && senha == senhaUsuario.value) {
+        alert("Senha Correta");
+    } else {
+        alert("Senha Incorreta");
+    }
 })
